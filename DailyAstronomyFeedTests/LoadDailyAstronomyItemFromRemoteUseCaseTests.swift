1bf1d4c7685daa7a762c0423b66fe9bfbccc45cd
AE <aeiosdev@gmail.com>
2023-03-14 23:33:03 -0500

    RemoteAstronomyLoader does not request data on init method

--- DailyAstronomyFeedTests/LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
+++ DailyAstronomyFeedTests/LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
@@ -6,8 +6,44 @@
 //
 
 import XCTest
+import DailyAstronomyFeed
+
+final class RemoteAstronomyLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
 
 final class LoadDailyAstronomyItemFromRemoteUseCaseTests: XCTestCase {
 
+    func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        let _ = RemoteAstronomyLoader(url: url, client: client)
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    // MARK: - Helpers
+    
+    private final class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
 
 }

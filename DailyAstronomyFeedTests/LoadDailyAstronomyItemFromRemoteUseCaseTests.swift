0180fed65059d626bcc85e82bf46a3ad65c53ebc
AE <aeiosdev@gmail.com>
2023-03-14 23:17:34 -0500

    Create 'LoadDailyAstronomyItemFromRemoteUseCastTests' file

---
+++ DailyAstronomyFeedTests/LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
+//  DailyAstronomyFeedTests
+//
+//  Created by Alvin Escobar on 3/14/23.
+//
+
+import XCTest
+
+final class LoadDailyAstronomyItemFromRemoteUseCaseTests: XCTestCase {
+
+
+}

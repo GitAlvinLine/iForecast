a6c6e9c7e6ed29c0b5506cb5404e5957e3e02ebb
AE <aeiosdev@gmail.com>
2023-03-18 20:23:58 -0500

    Delivers connectivity error on client error

--- DailyAstronomyFeedTests/LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
+++ DailyAstronomyFeedTests/LoadDailyAstronomyItemFromRemoteUseCaseTests.swift
@@ -33,6 +33,17 @@ final class LoadDailyAstronomyItemFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [anyURL(), anyURL()])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteDailyAstronomyLoader.Error?
+        sut.load { error in capturedError = error }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteDailyAstronomyLoader, client: HTTPClientSpy) {
@@ -43,8 +54,12 @@ final class LoadDailyAstronomyItemFromRemoteUseCaseTests: XCTestCase {
     
     private final class HTTPClientSpy: HTTPClient {
         var requestedURLs: [URL] = []
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }

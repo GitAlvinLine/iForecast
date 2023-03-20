12bd5d1c7f9357624f383f2653147763bbb1b0e2
AE <aeiosdev@gmail.com>
2023-03-20 17:06:57 -0500

    Extract test setup to a helper method

--- DailyAstronomyFeedAPIEndToEndTests/DailyAstronomyFeedAPIEndToEndTests.swift
+++ DailyAstronomyFeedAPIEndToEndTests/DailyAstronomyFeedAPIEndToEndTests.swift
@@ -11,7 +11,24 @@ import DailyAstronomyFeed
 final class DailyAstronomyFeedAPIEndToEndTests: XCTestCase {
     
     func test_endToEndServerGETAstronomyItemResult_hasData() {
-        let apiKey = "Vw08T8hJGYTwWTzHnqOygQTxXAnVhU29HmE2Bcij"
+        switch getAstronomyItemResult() {
+        case let .success(astronomyItem)?:
+            XCTAssertFalse(astronomyItem.date.isEmpty, "Expected to have a value for the date, but has no value.")
+            XCTAssertFalse(astronomyItem.explanation.isEmpty, "Expected to have a value for explanation, but has no value.")
+            XCTAssertFalse(astronomyItem.imageURL.absoluteString.isEmpty , "Expected to have a value for the URL, but has no URL absolute path.")
+            XCTAssertFalse(astronomyItem.title.isEmpty, "Expected to have a value for the title, but has no value.")
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful astronomy item result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful astronomy item result, got no result instead")
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func getAstronomyItemResult() -> DailyAstronomyResult? {
         let serverURL = URL(string: "https://api.nasa.gov/planetary/apod?api_key=\(apiKey)")!
         let client = URLSessionHTTPClient()
         let loader = RemoteDailyAstronomyLoader(url: serverURL, client: client)
@@ -25,18 +42,10 @@ final class DailyAstronomyFeedAPIEndToEndTests: XCTestCase {
         }
         wait(for: [exp], timeout: 15.0)
         
-        switch receivedResult {
-        case let .success(astronomyItem)?:
-            XCTAssertFalse(astronomyItem.date.isEmpty, "Expected to have a value for the date, but has no value.")
-            XCTAssertFalse(astronomyItem.explanation.isEmpty, "Expected to have a value for explanation, but has no value.")
-            XCTAssertFalse(astronomyItem.imageURL.absoluteString.isEmpty , "Expected to have a value for the URL, but has no URL absolute path.")
-            XCTAssertFalse(astronomyItem.title.isEmpty, "Expected to have a value for the title, but has no value.")
-            
-        case let .failure(error)?:
-            XCTFail("Expected successful astronomy item result, got \(error) instead")
-            
-        default:
-            XCTFail("Expected successful astronomy item result, got no result instead")
-        }
+        return receivedResult
+    }
+    
+    private var apiKey: String {
+        return "Vw08T8hJGYTwWTzHnqOygQTxXAnVhU29HmE2Bcij"
     }
 }
